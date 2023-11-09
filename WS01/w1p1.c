f80df24244aefd9747975febe40d354b56a735dd
Nathan <mhk.nathan@gmail.com>
2023-11-09 18:01:24 -0500

    Workshop 1

---
+++ WS01/w1p1.c
@@ -0,0 +1,37 @@
+/*
+*****************************************************************************
+			<assessment name example: Workshop - #1 (Part-1)>
+Full Name : Nathan Kong
+Student ID#: 150950236
+Email : nkong@myseneca.ca
+Section : NDD
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/
+
+#include<stdio.h>
+
+int main() {
+    printf("Workshop 1 Part-1\n");
+    printf("=================\n\n");
+
+    printf("I'm displaying this using the 'printf' stdio\n");
+    printf("(standard input output) library function!\n\n");
+    
+    printf("Dear teacher,\n\n");
+
+    printf("  I promise I will work hard from this day onward.\n");
+    printf("  I acknowledge that practice is extremely important,\n");
+    printf("  so I will do all workshops, quizzes, and assignments.\n\n");
+
+    printf("Sincerely,\n\n");
+
+    printf("Nathan Kong\n");
+    printf("Student ID# 150950236\n");
+
+    return 0;
+}
